3898ba49e073aba330523d5b5abb880803960a12
Pierrick Roger <pk.roger@icloud.com>
2022-05-08 20:24:21 +0200

    Add all libs.

--- all.sh
+++ all.sh
@@ -1,4 +1,8 @@
 source "$(dirname $BASH_SOURCE)/argparse.sh"
+source "$(dirname $BASH_SOURCE)/array.sh"
+source "$(dirname $BASH_SOURCE)/deprecate.sh"
 source "$(dirname $BASH_SOURCE)/logging.sh"
 source "$(dirname $BASH_SOURCE)/os.sh"
+source "$(dirname $BASH_SOURCE)/runtime.sh"
 source "$(dirname $BASH_SOURCE)/str.sh"
+source "$(dirname $BASH_SOURCE)/textfile.sh"
