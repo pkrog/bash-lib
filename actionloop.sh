19f72cb956d29a59f73fb75f9baa0dbf5e679177
Pierrick Roger <pk.roger@icloud.com>
2022-05-08 22:31:43 +0200

    Create library file.

---
+++ actionloop.sh
@@ -0,0 +1,12 @@
+# The name/prefix of this module is AL (Action Loop):
+#   "al_"  for the public functions.
+#   "AL_"  for the public global variables or constants.
+#   "_al_" for the private functions.
+#   "_AL_" for the private global variables or constants.
+
+# Include guard
+if [[ -z $_BASH_LIB_EMBEDDED ]] ; then
+	[[ -z $_AL_SOURCED ]] || return 0
+	_AL_SOURCED=1
+fi
+
